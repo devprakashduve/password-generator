b7c2537b3cfa93c90db365b11a7926476cab1467
devprakashduve <dev876.1@gmail.com>
2023-10-29 18:51:10 +0530

    currency converter done

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/devd/projects/Nextjs/password-generator/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- src/app/currency-converter/page.tsx
+++ src/app/currency-converter/page.tsx
@@ -56,10 +56,10 @@ const CurrencyConverter = () => {
   return (
     <>
       <div
-        className="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-14 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"
+        className="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-14 flex justify-center items-center inset-0 z-5 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"
         id="modal-id"
       >
-        <div className="absolute bg-white opacity-20 inset-0 z-0"></div>
+        <div className="absolute bg-white opacity-5 inset-0 z-0"></div>
         <div className="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg border border-gray-300">
           <h2 className="text-xl text-center font-bold py-4 " key="h21">
             Currency Converter
