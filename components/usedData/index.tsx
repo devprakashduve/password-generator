b497bf377cf87e5a1622cebd57cee54f64d4a84f
devprakashduve <dev876.1@gmail.com>
2023-11-12 13:09:35 +0530

    continue..

--- components/usedData/index.tsx
+++ components/usedData/index.tsx
@@ -6,7 +6,9 @@ const UserData=()=>{
     const {data}=useContext(UserContext);
     
 return (
-    <div className="user-data text-center font-bold py-4 "><p>You loggedin as : {data.email}</p>
+    <div className="user-data text-center font-bold py-4 ">
+        <h1>Profile Component</h1>
+        <p className="text-green-500">{data.email? "Welcome "+data.email:""}</p>
     {/* <p>User Password: {data.password}</p> */}
     </div>
 )
