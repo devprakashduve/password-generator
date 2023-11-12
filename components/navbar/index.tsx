bc84c07780616feca32b764f27afa7fd408589b4
devprakashduve <dev876.1@gmail.com>
2023-11-12 14:07:01 +0530

    login page functionality

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -104,12 +104,12 @@ const NavBar=()=> {
                     ))}
                   </nav>
                 </div>
-                <button
+                {/* <button
                   type="button"
                   className="mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                 >
                   Button text
-                </button>
+                </button> */}
               </div>
             </div>
           </div>
